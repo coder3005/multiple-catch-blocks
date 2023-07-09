4f4b6feacab2250095960e4b0c26798ed73e72e6
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-09 11:51:59 +0530

    Create mc6.java

---
+++ mc6.java
@@ -0,0 +1,24 @@
+/* multiple catch blocks */
+class multicatch
+{
+    public static void main(String[] args) {
+        
+        try
+        {
+            int a=10,b=2,c;
+            c=a/b;
+            System.out.println(c);
+
+            int arr[]={10,20,30};
+            System.out.println(arr[4]);
+
+            String str=null;
+            System.out.println(str.toUpperCase());
+        }
+        catch(Exception x)
+        {
+            System.out.println("All type Exception handled");
+        }
+        // Exception x=new NullPointerException();
+    }
+}
