fe179a9c485eacb63effb07af86a4066b1331ab9
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-09 11:44:08 +0530

    Create mc1.java

---
+++ mc1.java
@@ -0,0 +1,35 @@
+/* multiple catch blocks */
+class multicatch
+{
+    public static void main(String[] args) {
+        
+        try
+        {
+            int a=10,b=0,c;
+            c=a/b;
+            System.out.println(c);
+
+            int arr[]={10,20,30};
+            System.out.println(arr[0]);
+
+            String str="abhi";
+            System.out.println(str.toUpperCase());
+        }
+        catch(ArrayIndexOutOfBoundsException a)
+        {
+            System.out.println("Array Exception");
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Arithmetic Exception");
+        }
+        catch(NumberFormatException n)
+        {
+            System.out.println("Number Exception");
+        }
+        catch(Exception x)
+        {
+            System.out.println("All type Exception handled");
+        }
+    }
+}
